8c262a47c1a9540e1d9798696e02f5c6558b3447
b21matfi <b21matfi@student.his.se>
2022-04-12 15:14:11 +0200

    Added button, connected button to another java so when pressed it switches to that one.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -2,13 +2,64 @@ package com.example.widgets;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button Signin;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Signin = findViewById(R.id.myButton);
+        Signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "MainActivity button pressed");
+
+                Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
-}
+
+
+
+    public MainActivity() {
+        super();
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        Log.d("==>", "MainActivity Resumeed");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("==>", "MainActivity Started");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("==>", "MainActivity stopped");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("==>", "MainActivity destroyed");
+    }
+
+
+    }
+
---
+++ app/src/main/java/com/example/widgets/ProfileActivity.java
@@ -0,0 +1,4 @@
+package com.example.widgets;
+
+public class ProfileActivity {
+}
