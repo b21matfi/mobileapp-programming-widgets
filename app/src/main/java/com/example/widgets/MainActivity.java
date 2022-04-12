977be2cb588c53529b20a68236654ff13a519870
b21matfi <b21matfi@student.his.se>
2022-04-12 16:07:37 +0200

    Added a new button for sign out, added so that someone can use their username to signin

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -7,24 +7,32 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
     private Button Signin;
-
+    private EditText Username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+        Username = findViewById(R.id.myUsername);
         Signin = findViewById(R.id.myButton);
         Signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Log.d("==>", "MainActivity button pressed");
+                Log.d("==>", "Username"+Username.getText().toString());
+
+
 
                 Intent intent = new Intent(MainActivity.this,ProfileActivity.class);
+                startActivity(intent);
+
+                intent.putExtra("Username",Username.getText().toString());
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/widgets/ProfileActivity.java
+++ app/src/main/java/com/example/widgets/ProfileActivity.java
@@ -1,4 +1,59 @@
 package com.example.widgets;
 
-public class ProfileActivity {
-}
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class ProfileActivity extends AppCompatActivity {
+    private Button Signout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        Signout = findViewById(R.id.myButton_Signout);
+
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        String username = extras.getString("Username");
+
+
+        Signout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>", "ProfileActivity button pressed");
+                finish();
+
+            }
+        });
+    }
+        @Override
+        protected void onPostResume() {
+            super.onPostResume();
+            Log.d("==>", "ProfileActivity Resumeed");
+        }
+
+        @Override
+        protected void onStart() {
+            super.onStart();
+            Log.d("==>", "ProfileActivity Started");
+        }
+
+        @Override
+        protected void onStop() {
+            super.onStop();
+            Log.d("==>", "ProfileActivity stopped");
+        }
+
+        @Override
+        protected void onDestroy() {
+            super.onDestroy();
+            Log.d("==>", "ProfileActivity destroyed");
+        }
+    }
